fb8eb7b010bc0547e3dc128cbfe89afdcfe736b3
Evgeny Novikov <acidjoe1986@gmail.com>
2016-04-20 21:11:04 +0500

    Create core logic for Snake

--- Assets/Games/Snake/Scripts/Snake.cs
+++ Assets/Games/Snake/Scripts/Snake.cs
@@ -1,15 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Snake : MonoBehaviour {
+public class Snake : MonoBehaviour
+{
+    public GameObject snakeBit;
 
-	// Use this for initialization
-	void Start () {
-	
+    public enum State { left, right, up, down}
+
+    public State currentState;
+
+    public Vector2 dir;
+
+    bool isChoosen;
+    Vector2 prevPos, nextPos;
+    Quaternion prevRot;
+
+    public float spawnTime;
+
+    Queue<GameObject> bits;
+
+    public int lenght;
+
+    void Start ()
+    {
+        spawnTime = 0.2f;
+        bits = new Queue<GameObject>();
+        lenght = 3;
+        setState(State.up);
+        Invoke("MoveSnake", spawnTime);
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+    void MoveSnake()
+    {
+        isChoosen = false;
+        prevPos = transform.position;
+        prevRot = transform.rotation;
+        nextPos = prevPos + dir;
+        if (nextPos.x > 15.0f)
+        {
+            nextPos.x = -15.0f;
+        }
+        if (nextPos.x < -15.0f)
+        {
+            nextPos.x = 15.0f;
+        }
+        if (nextPos.y > 10.0f)
+        {
+            nextPos.y = -10.0f;
+        }
+        if (nextPos.y < -10.0f)
+        {
+            nextPos.y = 10.0f;
+        }
+
+        transform.position = nextPos;
+
+        createBit();
+        Invoke("MoveSnake", spawnTime);
+    }
+
+    void createBit()
+    {
+        var newBit = Instantiate(snakeBit, prevPos, prevRot);
+        GameObject bit = newBit as GameObject;
+        bits.Enqueue(bit);
+
+        if (bits.Count > lenght)
+        {
+            Destroy(bits.Dequeue());
+        }
+    }
+
+    void setState(State state)
+    {
+        switch (state)
+        {
+            case State.up:
+                dir = Vector2.up;
+                break;
+            case State.down:
+                dir = -Vector2.up;
+                break;
+            case State.left:
+                dir = -Vector2.right;
+                break;
+            case State.right:
+                dir = Vector2.right;
+                break;
+        }
+        currentState = state;
+    }
 }
