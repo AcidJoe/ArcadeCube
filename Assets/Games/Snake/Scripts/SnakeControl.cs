975ce6bc87612582be975e1c2c12a2a8c88a84eb
Evgeny Novikov <acidjoe1986@gmail.com>
2016-04-20 21:41:23 +0500

    Add ISnakeController interface

--- Assets/Games/Snake/Scripts/SnakeControl.cs
+++ Assets/Games/Snake/Scripts/SnakeControl.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SnakeControl : MonoBehaviour {
+public class SnakeControl : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Update ()
+    {
 	
 	}
 }
