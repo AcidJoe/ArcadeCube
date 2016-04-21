df1a14c71b9133b4ed39695c8b4f2f4311dfb26c
Evgeny Novikov <acidjoe1986@gmail.com>
2016-04-21 20:03:20 +0500

    Add more Controller types

---
+++ Assets/Games/Snake/Scripts/SnakeAccel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeAccel : MonoBehaviour, ISnakeController
+ {
+    public int _direction = 0;
+
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    public void SetDirection(int i)
+    {
+        _direction = i;
+    }
+
+    public int GetDirection()
+    {
+        return _direction;
+    }
+}
