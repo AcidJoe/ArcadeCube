fd73504fb01f393943b5567991dce465eff93e7e
Evgeny Novikov <acidjoe1986@gmail.com>
2016-04-21 21:30:27 +0500

    Start develop accelerometer Control.

--- Assets/Games/Snake/Scripts/SnakeAccel.cs
+++ Assets/Games/Snake/Scripts/SnakeAccel.cs
@@ -5,6 +5,8 @@ public class SnakeAccel : MonoBehaviour, ISnakeController
  {
     public int _direction = 0;
 
+    Vector3 accel;
+
     void Start ()
     {
 	
@@ -12,7 +14,7 @@ public class SnakeAccel : MonoBehaviour, ISnakeController
 	
 	void Update ()
     {
-	
+        AccelInput();
 	}
 
     public void SetDirection(int i)
@@ -23,5 +25,35 @@ public class SnakeAccel : MonoBehaviour, ISnakeController
     public int GetDirection()
     {
         return _direction;
+    }
+
+    public void AccelInput()
+    {
+        accel = Input.acceleration;
+
+        if (Mathf.Abs(accel.y) > Mathf.Abs(accel.z) && accel.y > 0.2f)
+        {
+            SetDirection(2);
+        }
+
+        else if (Mathf.Abs(accel.y) > Mathf.Abs(accel.z) && accel.y < -0.2f)
+        {
+            SetDirection(8);
+        }
+
+        else if (Mathf.Abs(accel.y) < Mathf.Abs(accel.z) && accel.y > 0.2f)
+        {
+            SetDirection(6);
+        }
+
+        else if (Mathf.Abs(accel.y) < Mathf.Abs(accel.z) && accel.y < -0.2f)
+        {
+            SetDirection(4);
+        }
+
+        else if(Mathf.Abs(accel.y) < 0.2f && Mathf.Abs(accel.z)< 0.2f)
+        {
+            SetDirection(0);
+        }
     }
 }
